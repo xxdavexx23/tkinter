d00a1a20244f0718c009e22f605133953d4dd917
xxdavexx23 <dxc783@miami.edu>
2021-04-27 14:46:56 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter
+
+
+window = tkinter.Tk()
+
+window.title("hello")
+
+'''def say_hi():
+    tkinter.Label(window, text = "Hi").pack()
+
+tkinter.Label(window, text="Click Me!", command = say_hi).pack()'''
+window.mainloop()
